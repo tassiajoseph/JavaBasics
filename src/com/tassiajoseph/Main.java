9163897aabef3a318df34e8fdbe84e72200c0a01
Tassia Joseph <tassiajoseph.student@careerdevs.com>
2022-01-06 10:43:41 -0500

    update main

--- src/com/tassiajoseph/Main.java
+++ src/com/tassiajoseph/Main.java
@@ -5,7 +5,16 @@ public class Main {
     public static void main(String[] args) {
 	    String firstName = "Tassia";
         String lastName = "Joseph";
-        String fullName = firstName + " " + lastName;
-        System.out.println(fullName);
+        String fullName1 = firstName + " " + lastName;
+        System.out.println( fullName1 );
+
+        System.out.println( createFullName("Tassia", "Joseph") );
     }
+
+    // createFullName method
+    public static String createFullName(String firstName, String lastName) {
+        String fullName2 = firstName + " " + lastName;
+        return fullName2;
+    }
+
 }
